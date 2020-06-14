c01f58c80629b256bff94a3fae3d813c8c62fe9a
alishazia <shaziaali812@gmail.com>
2020-06-14 11:20:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-lof4bw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-lof4bw)
---
+++ assests/vendor.js
@@ -0,0 +1,8 @@
+const userInput = document.getElementById("input-number");
+const addBtn = document.getElementById("add-btn");
+const subtractBtn = document.getElementById("sub-btn");
+const multiplyBtn = document.getElementById("mul-btn");
+const divideBtn = document.getElementById("div-btn");
+
+const currentResultOutput = document.getElementById("current-result");
+const currentCalculationOutput = document.getElementById("current-calculation");
